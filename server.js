8a907e830fa8c0640ea32473cc9201013c8fedc1
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-18 18:54:50 +0000

    setup package.json for heroku deployment

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // connect database
-connectDB();
+// connectDB();
 
 // Init Middleware
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('API Running'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 //define routes
 // TBD
 
