3c118f61a4e915ca3da86e8391c41f7dcb418891
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-18 21:29:59 +0000

    feat: add navbar and landing page to the App file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 
 function App() {
   return (
     <div className='App'>
-      <h2>StocksApp</h2>
+      <Router>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+      </Router>
     </div>
   );
 }
