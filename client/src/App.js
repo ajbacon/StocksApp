07df8000f13da8b56125cfbed3753dcb70159bbd
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-19 08:06:30 +0000

    feat: create login form component. chore: render both login and register forms to app

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Router>
         <Navbar />
         <Route exact path='/' component={Landing} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
       </Router>
     </div>
   );
