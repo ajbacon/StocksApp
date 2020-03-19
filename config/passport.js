2c2f318966fa2d234f184c999871d13a08d5ec21
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-19 20:05:04 +0000

    fix: add env variables for production

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const keys = require('../config/keys');
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secretOrKey;
+opts.secretOrKey = process.env.JWT_SECRET || keys.secretOrKey;
 module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
