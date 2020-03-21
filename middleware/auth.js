eb908ac4c56470ec41e54d021bed1fadde0c9289
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-21 08:28:41 +0000

    feat: create jwt auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
+
+module.exports = function(req, res, next) {
+  // get token from the header
+  const token = req.header('x-auth-token');
+
+  // if no token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorisation denied' });
+  }
+
+  //verify token
+  try {
+    const decoded = jwt.verify(token, keys.secretOrKey);
+    req.user = decoded.user;
+  } catch (err) {}
+};
