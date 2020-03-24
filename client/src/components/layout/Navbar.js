ba2135aab65596a0fb1bb91de0b6ded17a1a7f05
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-24 10:32:31 +0000

    feat: switch navbar links whether logged in or not

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,38 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { logout } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Navbar = () => {
+//redux
+import { connect } from 'react-redux';
+import { logout } from '../../actions/auth';
+
+const Navbar = ({ auth, logout }) => {
+  const { isAuthenticated, loading } = auth;
+  const authLinks = (
+    <ul className='right hide-on-med-and-down'>
+      <li>
+        <Link onClick={logout} to='#!' className='black-text'>
+          Logout
+        </Link>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul className='right hide-on-med-and-down'>
+      <li>
+        <Link to='/register' className='black-text'>
+          Register
+        </Link>
+      </li>
+      <li>
+        <Link to='/login' className='black-text'>
+          Login
+        </Link>
+      </li>
+    </ul>
+  );
+
   return (
     <Fragment>
       <nav>
@@ -17,17 +45,23 @@ const Navbar = () => {
             <i className='material-icons'>trending_up</i>
             StocksApp
           </Link>
-          <ul className='right hide-on-med-and-down'>
-            <li>
-              <Link to='/' className='black-text'>
-                Logout
-              </Link>
-            </li>
-          </ul>
+          {!loading && (
+            <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+          )}
         </div>
       </nav>
       ;
     </Fragment>
   );
 };
-export default Navbar;
+
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
