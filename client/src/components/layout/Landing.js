a4191b51d34d88efae4d1338559db12e929c0ebf
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-24 09:21:32 +0000

    chore: update to function component

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Landing = () => {
+// redux
+import { connect } from 'react-redux';
+
+const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
     return <Redirect to='/dashboard' />;
   }
@@ -47,4 +51,12 @@ const Landing = () => {
     </div>
   );
 };
-export default Landing;
+
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+export default connect(mapStateToProps)(Landing);
