6c035bb776471141fa126642d0bffc4d4f982280
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-27 08:21:14 +0000

    chore: config jest. Add first test for user registration end point

--- test/server.test.js
+++ test/server.test.js
@@ -1,13 +1,33 @@
 const supertest = require('supertest');
+const mongoose = require('mongoose');
 const app = require('../server');
 const request = supertest(app);
+const User = require('../models/User');
 
 describe('server', () => {
-  describe('GET /', () => {
-    xit('should return a response status of 200', async () => {
-      const response = await request.get('/');
+  beforeEach(async done => {
+    await User.deleteMany();
+    done();
+  });
+
+  afterAll(async done => {
+    mongoose.connection.close();
+    done();
+  });
+
+  describe('POST /api/users/register', () => {
+    it('should return a response status of 200', async done => {
+      const testUser = {
+        firstName: 'test',
+        surname: 'user',
+        email: 'test_user@example.com',
+        password: 'pass123',
+        password2: 'pass123'
+      };
+      const response = await request.post('/api/users/register').send(testUser);
 
       expect(response.status).toEqual(200);
+      done();
     });
   });
 });
