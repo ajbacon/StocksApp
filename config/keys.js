622f157e233aa9a2f856c53fe6b92e461ea55865
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-27 13:23:15 +0000

    fix: debugging travis test issue

--- config/keys.js
+++ config/keys.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'production') {
 } else if (process.env.NODE_ENV === 'test') {
   console.log(process.env.NODE_ENV);
   // dev set of keys
-  module.exports = require('./test');
+  module.exports = require('./testenv');
 } else {
   console.log(process.env.NODE_ENV);
   // dev set of keys
---
+++ config/testenv.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://localhost:27017/stocks-app-test',
+  secretOrKey: 'SECRET'
+};
