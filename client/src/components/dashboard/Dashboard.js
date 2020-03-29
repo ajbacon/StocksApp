6fe7424880afccad6d3911acea587ad75a397ebb
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-29 22:29:11 +0100

    feat: add search box

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,7 +7,31 @@ import { connect } from 'react-redux';
 const capitalize = require('../../utils/capitalize');
 
 const Dashboard = ({ auth: { user } }) => {
-  return <div>Welcome, {user && capitalize(user.firstName)}</div>;
+  return (
+    <div>
+      <div>Welcome, {user && capitalize(user.firstName)}</div>
+      <div className='center row '>
+        <div className='card col s6'>
+          <div>
+            <div className='nav-wrapper'>
+              <div className='input-field'>
+                <input
+                  id='search'
+                  type='search'
+                  placeholder='search...'
+                  required
+                />
+                <label className='label-icon' htmlFor='search'>
+                  <i className='material-icons'>search</i>
+                </label>
+                <i className='material-icons'>close</i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Dashboard.propTypes = {
