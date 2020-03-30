ca24f6ba9b52a6468e759a7b5bec62d261231bfa
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-30 23:06:00 +0100

    feat: list search items

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 //redux
@@ -8,14 +8,29 @@ const fuzzysort = require('fuzzysort');
 const symbolsUS = require('../../config/US');
 const capitalize = require('../../utils/capitalize');
 
+const anArr = ['Andrew', 'Peta', 'Pebble', 'tiger'];
+
 const Dashboard = ({ auth: { user } }) => {
+  const [searchData, setSearchData] = useState({
+    search: []
+  });
+
   const searchCode = e => {
     const results = fuzzysort.go(e.target.value, symbolsUS, {
-      key: 'symbol',
-      limit: 10
+      key: 'description',
+      limit: 10,
+      threshold: -500
     });
+    setSearchData({ ...searchData, search: results });
     console.log(results);
   };
+
+  const searchList = () => {
+    return searchData.search.map(item => {
+      return <div>{item.obj.description}</div>;
+    });
+  };
+
   return (
     <div>
       <div>Welcome, {user && capitalize(user.firstName)}</div>
@@ -40,6 +55,7 @@ const Dashboard = ({ auth: { user } }) => {
           </div>
         </div>
       </div>
+      <div>{searchList()}</div>
     </div>
   );
 };
