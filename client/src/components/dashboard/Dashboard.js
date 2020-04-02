000301fd401c8e53a044c6e35b43092fb1fe8405
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-04-02 20:11:51 +0100

    refactor: split api quote call into a useEffect hook

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
+import CompanyData from './CompanyData';
 import PropTypes from 'prop-types';
 import Classes from './Dashboard.module.css';
-import axios from 'axios';
+// import axios from 'axios';
 //redux
 import { connect } from 'react-redux';
-import { disconnect } from 'mongoose';
 
 const fuzzysort = require('fuzzysort');
 const symbolsUS = require('../../config/US');
 const capitalize = require('../../utils/capitalize');
 
 // todo:
-// Split up into multiple components
-// change to async await?
+// Split out company data component - done
+// split out search bar component
 // improve search bar such that search results can be selected using the arrow keys
 // improve visually
 
@@ -20,27 +20,6 @@ const Dashboard = ({ auth: { user } }) => {
   const [search, setSearch] = useState([]);
   const [searchFocus, setSearchFocus] = useState(false);
   const [companyData, setCompanyData] = useState([]);
-  const [currentQuote, setCurrentQuote] = useState([]);
-
-  let isFirstRun = useRef(true);
-  useEffect(() => {
-    if (isFirstRun.current) {
-      isFirstRun.current = false;
-      return;
-    }
-    console.log('here');
-    console.log(companyData.symbol);
-    let url = `https://finnhub.io/api/v1/quote?symbol=${companyData[0].symbol}&token=${process.env.REACT_APP_FINNHUB_API_KEY}`;
-
-    fetch(url)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setSearchFocus(false);
-        setCurrentQuote([data]);
-      });
-  }, [companyData]);
 
   const searchCode = e => {
     const searchResults = fuzzysort.go(e.target.value, symbolsUS, {
@@ -49,11 +28,11 @@ const Dashboard = ({ auth: { user } }) => {
       threshold: -500
     });
     setSearch(searchResults);
-    console.log(searchResults);
   };
 
   const selectResult = obj => {
     setCompanyData([obj]);
+    setSearchFocus(false);
   };
 
   const searchList = () => {
@@ -82,15 +61,9 @@ const Dashboard = ({ auth: { user } }) => {
 
   return (
     <div>
-      <h3>
-        Welcome,{' '}
-        {user &&
-          `${capitalize(user.firstName)}${
-            capitalize(user.firstName) === 'Niel' ? ' (poes)' : ''
-          }`}
-      </h3>
+      <h3>Welcome, {user && capitalize(user.firstName)}</h3>
       <div className='row'>
-        <div className='card col s6'>
+        <div className='card col s12'>
           <div>
             <div className='nav-wrapper'>
               <div className='input-field'>
@@ -119,26 +92,9 @@ const Dashboard = ({ auth: { user } }) => {
           </div>
         </div>
       </div>
-      <h4>{companyData.length > 0 ? companyData[0].description : ''}</h4>
-      <div>
-        {currentQuote.length > 0 ? `Current Price: ${currentQuote[0].c}` : ''}
-      </div>
-      <div>
-        {currentQuote.length > 0
-          ? `Day Opening Price: ${currentQuote[0].o}`
-          : ''}
-      </div>
-      <div>
-        {currentQuote.length > 0 ? `Day High Price: ${currentQuote[0].h}` : ''}
-      </div>
-      <div>
-        {currentQuote.length > 0 ? `Day Low Price: ${currentQuote[0].l}` : ''}
-      </div>
-      <div>
-        {currentQuote.length > 0
-          ? `Previous Closing Price: ${currentQuote[0].pc}`
-          : ''}
-      </div>
+      {companyData.map((data, i) => (
+        <CompanyData key={i} companyData={data} />
+      ))}
     </div>
   );
 };
