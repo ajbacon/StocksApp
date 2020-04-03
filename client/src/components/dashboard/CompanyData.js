0b7cbb37be0bc2b1288f8298630833524d0679b4
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-04-03 13:05:06 +0100

    refactor: add loading element while api data is fetched

--- client/src/components/dashboard/CompanyData.js
+++ client/src/components/dashboard/CompanyData.js
@@ -5,8 +5,10 @@ import React, { useEffect, useState } from 'react';
 
 const CompanyData = ({ companyData }) => {
   const [currentQuote, setCurrentQuote] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     let url = `https://finnhub.io/api/v1/quote?symbol=${companyData.symbol}&token=${process.env.REACT_APP_FINNHUB_API_KEY}`;
 
     fetch(url)
@@ -15,31 +17,20 @@ const CompanyData = ({ companyData }) => {
       })
       .then(data => {
         setCurrentQuote([data]);
+        setLoading(false);
       });
   }, [companyData.symbol]);
 
-  return (
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
     <div>
       <h4>{companyData.description}</h4>
-      <div>
-        {currentQuote.length > 0 ? `Current Price: ${currentQuote[0].c}` : ''}
-      </div>
-      <div>
-        {currentQuote.length > 0
-          ? `Day Opening Price: ${currentQuote[0].o}`
-          : ''}
-      </div>
-      <div>
-        {currentQuote.length > 0 ? `Day High Price: ${currentQuote[0].h}` : ''}
-      </div>
-      <div>
-        {currentQuote.length > 0 ? `Day Low Price: ${currentQuote[0].l}` : ''}
-      </div>
-      <div>
-        {currentQuote.length > 0
-          ? `Previous Closing Price: ${currentQuote[0].pc}`
-          : ''}
-      </div>
+      <div>Current Price: {currentQuote[0].c}</div>
+      <div>Day Opening Price: {currentQuote[0].o}</div>
+      <div>Day High Price: {currentQuote[0].h}</div>
+      <div>Day Low Price: {currentQuote[0].l}</div>
+      <div>Previous Closing Price: {currentQuote[0].pc}</div>
     </div>
   );
 };
