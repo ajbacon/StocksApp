b5b7069d07c2188e470990e65d5c86d53ecf087b
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-04-03 08:25:06 +0100

    refactor: separate out searchbar into separate component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import CompanyData from './CompanyData';
+import SearchBar from '../search/SearchBar';
 import PropTypes from 'prop-types';
-import Classes from './Dashboard.module.css';
+// import Classes from './Dashboard.module.css';
 // import axios from 'axios';
 //redux
 import { connect } from 'react-redux';
 
-const fuzzysort = require('fuzzysort');
-const symbolsUS = require('../../config/US');
 const capitalize = require('../../utils/capitalize');
 
 // todo:
@@ -17,81 +16,12 @@ const capitalize = require('../../utils/capitalize');
 // improve visually
 
 const Dashboard = ({ auth: { user } }) => {
-  const [search, setSearch] = useState([]);
-  const [searchFocus, setSearchFocus] = useState(false);
   const [companyData, setCompanyData] = useState([]);
-
-  const searchCode = e => {
-    const searchResults = fuzzysort.go(e.target.value, symbolsUS, {
-      keys: ['description', 'symbol'],
-      limit: 10,
-      threshold: -500
-    });
-    setSearch(searchResults);
-  };
-
-  const selectResult = obj => {
-    setCompanyData([obj]);
-    setSearchFocus(false);
-  };
-
-  const searchList = () => {
-    return search.map((item, i) => {
-      return (
-        <div
-          className={Classes.resultItem}
-          onClick={() => selectResult(item.obj)}
-          key={i}
-        >
-          {item.obj.description}
-        </div>
-      );
-    });
-  };
-
-  const onSearchFocus = () => {
-    setSearchFocus(true);
-  };
-
-  const onSearchBlur = () => {
-    setTimeout(() => {
-      setSearchFocus(false);
-    }, 200);
-  };
 
   return (
     <div>
       <h3>Welcome, {user && capitalize(user.firstName)}</h3>
-      <div className='row'>
-        <div className='card col s12'>
-          <div>
-            <div className='nav-wrapper'>
-              <div className='input-field'>
-                <input
-                  id='search'
-                  type='search'
-                  placeholder='search...'
-                  required
-                  onChange={e => searchCode(e)}
-                  onFocus={() => onSearchFocus()}
-                  onBlur={() => onSearchBlur()}
-                  autoComplete='off'
-                />
-                <label className='label-icon' htmlFor='search'>
-                  <i className='material-icons'>search</i>
-                </label>
-                <i className='material-icons'>close</i>
-              </div>
-              <div
-                className={`${Classes.searchResult} ${!searchFocus &&
-                  Classes.hideElement}`}
-              >
-                {searchList()}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SearchBar setCompanyData={setCompanyData} />
       {companyData.map((data, i) => (
         <CompanyData key={i} companyData={data} />
       ))}
