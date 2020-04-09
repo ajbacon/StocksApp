c889c350f2b4bb97dbb016294c8170284ca88fd5
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-04-09 15:52:45 +0100

    test: successful login action. Unsuccessful register action

--- client/src/_integrationTests/redux/authRedux.test.js
+++ client/src/_integrationTests/redux/authRedux.test.js
@@ -2,7 +2,7 @@ import moxios from 'moxios';
 import thunk from 'redux-thunk';
 
 import { storeFactory } from '../../utils/testUtils';
-import { register, loadUser } from '../../actions/auth';
+import { login, register, loadUser } from '../../actions/auth';
 
 const newUser = {
   firstName: 'Eric',
@@ -10,6 +10,11 @@ const newUser = {
   email: 'e_cantona@example.com',
   password: 'pass123',
   password2: 'pass123',
+};
+
+const existingUser = {
+  email: 'e_cantona@example.com',
+  password: 'pass123',
 };
 
 describe('register auth action creator', () => {
@@ -20,7 +25,7 @@ describe('register auth action creator', () => {
     moxios.uninstall();
   });
 
-  describe('successful authentication', () => {
+  describe('successful registration', () => {
     it('changes token, isAuthenticated and loading to authenticated state', async () => {
       const store = storeFactory();
 
@@ -42,6 +47,67 @@ describe('register auth action creator', () => {
       // the first for the /api/users/register
       // the second for /api/auth after dispatch(loadUser()) is called
       expect(moxios.requests.__items[0].config.url).toBe('/api/users/register');
+      expect(moxios.requests.__items[1].config.url).toBe('/api/auth');
+    });
+  });
+
+  describe('unsuccessful registration', () => {
+    it('does something', async () => {
+      const store = storeFactory();
+      moxios.stubRequest('/api/users/register', {
+        status: 400,
+        response: {
+          alert1: 'error message 1',
+          alert2: 'error message 2',
+        },
+      });
+
+      const result = await store.dispatch(register(newUser));
+      const newState = store.getState();
+      expect(newState.alert.length).toBe(2);
+      expect(newState.alert[0].msg).toBe('error message 1');
+      expect(newState.alert[1].msg).toBe('error message 2');
+
+      expect(newState.auth.token).toBe(null);
+      expect(newState.auth.isAuthenticated).toBe(false);
+      expect(newState.auth.loading).toBe(false);
+
+      console.log(newState);
+    });
+  });
+});
+
+describe('login auth action creator', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  describe('successful login', () => {
+    it('changes token, isAuthenticated and loading to authenticated state', async () => {
+      const store = storeFactory();
+
+      moxios.stubRequest('/api/auth', {
+        status: 200,
+        response: { token: 'jwtheader.payload.signature' },
+      });
+      moxios.stubRequest('/api/auth', {
+        status: 200,
+        response: { user: 'details' },
+      });
+
+      const result = await store.dispatch(login(existingUser));
+      const newState = store.getState();
+      console.log(newState);
+      expect(newState.auth.token).toBe('jwtheader.payload.signature');
+      expect(newState.auth.isAuthenticated).toBe(true);
+      expect(newState.auth.loading).toBe(false);
+      // check that the store.dispatch generates 2 moxios requests
+      // the first for the /api/users/register
+      // the second for /api/auth after dispatch(loadUser()) is called
+      expect(moxios.requests.__items[0].config.url).toBe('/api/auth');
       expect(moxios.requests.__items[1].config.url).toBe('/api/auth');
     });
   });
