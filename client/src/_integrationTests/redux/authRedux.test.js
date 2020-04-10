5c6b9c94553574b43a3bfbd10cd5a8b7245100f8
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-04-10 07:37:20 +0100

    test: load user unsuccessful transaction

--- client/src/_integrationTests/redux/authRedux.test.js
+++ client/src/_integrationTests/redux/authRedux.test.js
@@ -158,28 +158,33 @@ describe('load user auth action creator', () => {
     });
   });
 
-  // describe('unsuccessful registration', () => {
-  //   it('does something', async () => {
-  //     const store = storeFactory();
-  //     moxios.stubRequest('/api/users/register', {
-  //       status: 400,
-  //       response: {
-  //         alert1: 'error message 1',
-  //         alert2: 'error message 2',
-  //       },
-  //     });
+  describe('unsuccessful transaction', () => {
+    it('does something', async () => {
+      const initialState = {
+        alert: [],
+        auth: {
+          token: 'jwtheader.payload.signature',
+          isAuthenticated: true,
+          loading: false,
+          user: { user: 'details' },
+        },
+      };
+      const store = storeFactory(initialState);
+      console.log(store.getState());
+      moxios.stubRequest('/api/auth', {
+        status: 400,
+        response: {},
+      });
 
-  //     const result = await store.dispatch(register(newUser));
-  //     const newState = store.getState();
-  //     expect(newState.alert.length).toBe(2);
-  //     expect(newState.alert[0].msg).toBe('error message 1');
-  //     expect(newState.alert[1].msg).toBe('error message 2');
+      const result = await store.dispatch(loadUser());
+      const newState = store.getState();
 
-  //     expect(newState.auth.token).toBe(null);
-  //     expect(newState.auth.isAuthenticated).toBe(false);
-  //     expect(newState.auth.loading).toBe(false);
+      expect(newState.auth.token).toBe(null);
+      expect(newState.auth.isAuthenticated).toBe(false);
+      expect(newState.auth.loading).toBe(false);
+      expect(newState.auth.user).toBe(null);
 
-  //     console.log(newState);
-  //   });
-  // });
+      console.log(newState);
+    });
+  });
 });
