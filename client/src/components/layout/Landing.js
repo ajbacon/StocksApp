2481c0941a1845cad9e45de81c303f8ac3c623ca
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-04-13 16:41:01 +0100

    test: renders the landing page if user authorised, doesn't if not

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -10,7 +10,11 @@ const Landing = ({ isAuthenticated }) => {
     return <Redirect to='/dashboard' />;
   }
   return (
-    <div style={{ height: '75vh' }} className='container valign-wrapper'>
+    <div
+      data-test='component-landing'
+      style={{ height: '75vh' }}
+      className='container valign-wrapper'
+    >
       <div className='row'>
         <div className='col s12 center-align'>
           <h4>
@@ -26,7 +30,7 @@ const Landing = ({ isAuthenticated }) => {
               style={{
                 width: '140px',
                 borderRadius: '3px',
-                letterSpacing: '1.5px'
+                letterSpacing: '1.5px',
               }}
               className='btn btn-large waves-effect waves-light hoverable blue accent-3'
             >
@@ -39,7 +43,7 @@ const Landing = ({ isAuthenticated }) => {
               style={{
                 width: '140px',
                 borderRadius: '3px',
-                letterSpacing: '1.5px'
+                letterSpacing: '1.5px',
               }}
               className='btn btn-large waves-effect waves-light hoverable white black-text'
             >
@@ -53,10 +57,10 @@ const Landing = ({ isAuthenticated }) => {
 };
 
 Landing.propTypes = {
-  isAuthenticated: PropTypes.bool
+  isAuthenticated: PropTypes.bool,
 };
 
-const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
 });
 export default connect(mapStateToProps)(Landing);
