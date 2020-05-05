52117b8ae9c599b4be9a2037a2312b215cd9ecd4
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-05 19:44:49 +0100

    chore: remove production only code from test coverage

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/watchitems', watchItems);
 
+/* istanbul ignore next */
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
