817af3d27e2d205660135974fb2e486c7c9977f5
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-09 10:08:19 +0100

    test: successful mongoose connection

--- test/dbConnection.test.js
+++ test/dbConnection.test.js
@@ -4,9 +4,26 @@ const connectDB = require('../config/db');
 jest.mock('mongoose');
 
 describe('connectDB', () => {
+  it('should log success message to console upon successful connection', async () => {
+    const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
+    const mockConsoleLog = jest
+      .spyOn(console, 'log')
+      .mockImplementation(() => {});
+    const mongooseConnectSpyOn = jest
+      .spyOn(mongoose, 'connect')
+      .mockImplementation();
+
+    await connectDB();
+
+    expect(mongooseConnectSpyOn).toHaveBeenCalled();
+    expect(mockConsoleLog).toHaveBeenCalledWith(
+      'MongoDB successfully connected...'
+    );
+  });
+
   it('should exit with error code 1 with unsuccessful database connection', async () => {
     const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
-    const mockConsoleExit = jest
+    const mockConsoleError = jest
       .spyOn(console, 'error')
       .mockImplementation(() => {});
     const mongooseConnectSpyOn = jest
@@ -18,7 +35,7 @@ describe('connectDB', () => {
     await connectDB();
 
     expect(mongooseConnectSpyOn).toHaveBeenCalled();
-    expect(mockConsoleExit).toHaveBeenCalledWith('message');
+    expect(mockConsoleError).toHaveBeenCalledWith('message');
     expect(mockExit).toHaveBeenCalledWith(1);
   });
 });
