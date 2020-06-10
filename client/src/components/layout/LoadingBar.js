b4694ab3d1c3915e82050675a4e109f06ea9862b
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-10 22:35:31 +0100

    test: watchlist renders loading bar before data retrieved

--- client/src/components/layout/LoadingBar.js
+++ client/src/components/layout/LoadingBar.js
@@ -3,7 +3,10 @@ import Classes from './LoadingBar.module.css';
 
 function LoadingBar() {
   return (
-    <div className={`container ${Classes.loadingBar}`}>
+    <div
+      data-test='component-loading-bar'
+      className={`container ${Classes.loadingBar}`}
+    >
       <div className={`progress`}>
         <div className='indeterminate'></div>
       </div>
---
+++ client/src/components/watchlist/WatchList.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { storeFactory, findByTestAttr } from '../../utils/testUtils';
+import WatchList from './WatchList';
+
+const setup = (state = {}) => {
+  const store = storeFactory(state);
+  const wrapper = shallow(<WatchList store={store} />)
+    .dive()
+    .dive()
+    .dive();
+  return wrapper;
+};
+
+describe('<WatchList />', () => {
+  let wrapper;
+  let component;
+
+  it('renders loading bar before watchitems are retrieved', () => {
+    const initialState = {
+      auth: { isAuthenticated: true },
+    };
+    wrapper = setup(initialState);
+    component = findByTestAttr(wrapper, 'component-loading-bar');
+    expect(component).toHaveLength(1);
+  });
+});
