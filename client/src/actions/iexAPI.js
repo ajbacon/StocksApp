b4713e6c8789919da163f6ac0d9109b8f0f3dbee
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-07-07 13:50:30 +0100

    feat: insert function to extract data for applying to watchlist dropdowns

--- client/src/actions/iexAPI.js
+++ client/src/actions/iexAPI.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
-
 import getAlphaVantageKey from '../utils/apiLoadBalancer';
 import { SEARCH_QUOTE, SEARCH_QUOTE_ERROR } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
-const globalQuoteQuery = (symbol) => {
-  let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${getAlphaVantageKey()}`;
-  return axios.get(url);
-};
+const moment = require('moment');
+
 const globalQuoteQuery = (symbol) => {
   let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${getAlphaVantageKey()}`;
   return axios.get(url);
@@ -46,6 +43,21 @@ export const loadSearchQuote = (symbol) => async (dispatch) => {
       type: SEARCH_QUOTE_ERROR,
     });
   }
+  // reset defaut header
+  setAuthToken(localStorage.token);
+};
+
+export const loadWatchListData = (watchList) => async (dispatch) => {
+  delete axios.defaults.headers.common['x-auth-token'];
+
+  const payload = await Promise.all(
+    watchList.map(async (item) => {
+      const globalQuoteRes = await globalQuoteQuery(item.symbol);
+      const globalQuote = parseQuoteData(globalQuoteRes.data);
+      // const companyNewsRes =
+    })
+  );
+
   // reset defaut header
   setAuthToken(localStorage.token);
 };
